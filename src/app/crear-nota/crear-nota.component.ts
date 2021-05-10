73acf4502f25f149d565b5b70f78363c6aae5fe3
kataplan <dcatalanmarchese@gmail.com>
2021-05-10 13:36:05 -0400

    notas-Intento

--- src/app/crear-nota/crear-nota.component.ts
+++ src/app/crear-nota/crear-nota.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-crear-nota',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearNotaComponent implements OnInit {
 
-  constructor() { }
 
+  createFormGroup(){
+    return new FormGroup({
+      titulo:new FormControl('',[Validators.required, Validators.minLength(5)]),
+      estado:new FormControl('',[Validators.required, Validators.minLength(5)]),
+      texto:new FormControl('',[Validators.required ]),
+    
+    });
+  }
+  formulario:FormGroup;
+ 
+  constructor(){
+    this.formulario =this.createFormGroup();
+  }
   ngOnInit(): void {
+  }
+  
+  ValidarDatos(){
+    if(!this.formulario.valid){
+      alert("Complete el Formulario");
+      return;
+    }
+    if(this.formulario.value.genero==""){
+      alert("Debe elegir un genero");
+      return;
+    }
+    
   }
 
 }
+
+
